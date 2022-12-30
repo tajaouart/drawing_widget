aa685f3e35834a2054d6b4df2ede8a848d0fb566
TAJAOUART Mounir <tajaouartm@gmail.com>
2022-12-30 17:32:30 +0100

    added some widget testing

--- test/drawing_widget_test.dart
+++ test/drawing_widget_test.dart
@@ -1,12 +1,101 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:drawing_widget/drawing_widget.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('Test pump widget', (tester) async {
+    final drawing = Drawing();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: SizedBox(
+          width: 100,
+          height: 200,
+          child: DrawingWidget(
+            drawing: drawing,
+            height: 200,
+            width: 100,
+          ),
+        ),
+      ),
+    );
+
+    expect(drawing.points.isEmpty, true);
   });
+
+  testWidgets('Test drawing in drawing mode > isDrawing : true', (tester) async {
+    final drawing = Drawing();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: SizedBox(
+          width: 100,
+          height: 200,
+          child: DrawingWidget(
+            drawing: drawing,
+            height: 200,
+            width: 100,
+            isDrawing: true,
+          ),
+        ),
+      ),
+    );
+
+    await tester.drag(find.byType(DrawingWidget), const Offset(50.0, 50.0));
+
+    expect(drawing.points.isNotEmpty, true);
+  });
+
+  testWidgets('Test drawing in display mode > isDrawing : false', (tester) async {
+    final drawing = Drawing();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: SizedBox(
+          width: 100,
+          height: 200,
+          child: DrawingWidget(
+            drawing: drawing,
+            height: 200,
+            width: 100,
+            isDrawing: false,
+          ),
+        ),
+      ),
+    );
+
+    await tester.drag(find.byType(DrawingWidget), const Offset(50.0, 50.0));
+
+    expect(drawing.points.isEmpty, true);
+  });
+
+
+  testWidgets('Test drawing two paths', (tester) async {
+    final drawing = Drawing();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: SizedBox(
+          width: 100,
+          height: 200,
+          child: DrawingWidget(
+            drawing: drawing,
+            height: 200,
+            width: 100,
+            isDrawing: true,
+          ),
+        ),
+      ),
+    );
+
+    await tester.drag(find.byType(DrawingWidget), const Offset(50.0, 50.0));
+    await tester.drag(find.byType(DrawingWidget), const Offset(50.0, 50.0));
+
+    expect(drawing.points[0].isNotEmpty, true);
+    expect(drawing.points[1].isNotEmpty, true);
+  });
+
 }
+
+
